5c9e9dfe5897bd9b5d0ac9ce754308fe2de86f96
Jean Piffaut <40282095+JeanPiffaut@users.noreply.github.com>
2021-05-25 23:55:03 -0400

    Minor fixeds (#1)
    * Minor Fixed
    
    * Minor Fixed
    The connection data is added to the session variable

--- lib_database.php
+++ lib_database.php
@@ -6,12 +6,6 @@ use mysqli;
 use mysqli_result;
 
 /**
- * Library focused on facilitating and delivering the most complete system for executing queries to mysql databases.
- * @author Jean Piffaut - https://github.com/JeanPiffaut
- * @version 1.0.2
- */
-
-/**
  * The functions of this region focus on validating the processes and in case of error, grouping them in their own
  * review method.
  * The function to perform validations can be modified and in fact it is expected that something will be done to meet
@@ -154,6 +148,13 @@ function DBConnect(string $host, string $user, string $password, string $databas
             DBSetError(mysqli_connect_error(), mysqli_connect_errno());
             return false;
         } else {
+
+            $_SESSION['host'] = $host;
+            $_SESSION['user'] = $user;
+            $_SESSION['password'] = $password;
+            $_SESSION['database'] = $database;
+            $_SESSION['port'] = $port;
+            $_SESSION['socket'] = $socket;
 
             DBSetConnection($link);
             return $link;
